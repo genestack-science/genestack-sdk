907a495c11e8aa02d75b267a6f58309c31f479cc
genestack-science <saymen4166@gmail.com>
2026-02-21 10:36:56 +0530

    perf: optimize data cleaner cross-platform adapters calculation

---
+++ src/types/stacks.ts
@@ -0,0 +1,10 @@
+export interface CompiledCompound {
+  id: string;
+  name: string;
+  dosage: string;
+  frequency: string;
+  class: string;
+  pathway: string;
+  mappedExpressions: string[];
+  details: string;
+  contraindications: string[];
