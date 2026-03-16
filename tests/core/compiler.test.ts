52ed5182dc29734747eab8a50c92b9a6774d6a4d
genestack-science <saymen4166@gmail.com>
2026-03-16 12:47:27 +0530

    feat: define interfaces for sequencer

---
+++ tests/core/compiler.test.ts
@@ -0,0 +1,8 @@
+import { Compiler } from '../../src/core/compiler';
+import { ExpressionProfile } from '../../src/types/signals';
+
+describe('Compiler Unit Tests', () => {
+  const compiler = new Compiler();
+
+  const mockProfile: ExpressionProfile = {
+    id: 'expr_test_123',
