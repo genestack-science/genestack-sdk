34d3c613e14a9deb99346650e7e41311772c1c07
genestack-science <saymen4166@gmail.com>
2026-03-16 15:54:54 +0530

    fix: resolve issue in data cleaner processing

--- tests/adapters/blood.test.ts
+++ tests/adapters/blood.test.ts
@@ -2,3 +2,6 @@ import { BloodPanelParser } from '../../src/adapters/bloodPanel';
 
 describe('BloodPanelParser', () => {
   test('should parse blood markers correctly', () => {
+    const parser = new BloodPanelParser();
+    const mockContent = JSON.stringify([
+      { id: 'm1', name: 'Vitamin D', value: 45, unit: 'ng/mL', referenceRange: [30, 100] }
