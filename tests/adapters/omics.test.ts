a1f5d25edcf5525532b5ba30188e209ee27af739
genestack-science <saymen4166@gmail.com>
2026-04-02 22:24:04 +0530

    feat: add auto-discovery to sequencer

--- tests/adapters/omics.test.ts
+++ tests/adapters/omics.test.ts
@@ -3,3 +3,5 @@ import { MultiOmicsParser } from '../../src/adapters/omicsParser';
 describe('MultiOmicsParser', () => {
   test('should extract alleles for whitelisted variants', async () => {
     const parser = new MultiOmicsParser();
+    const mockContent = `
+rs4680 22 17300000 AA
